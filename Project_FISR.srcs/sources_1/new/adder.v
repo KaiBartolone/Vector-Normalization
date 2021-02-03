b1537121f3b7d4af7d5a98c01538676c28fd8235
KaiBartolone <kbartolo@stanford.edu>
2021-02-03 05:33:37 -0500

    initial implementation

--- Project_FISR.srcs/sources_1/new/adder.v
+++ Project_FISR.srcs/sources_1/new/adder.v
@@ -13,32 +13,37 @@ module adder(
     wire [7:0] le; // Lesser exponent
     wire [22:0] gm; // Greater mantissa
     wire [22:0] lm; // Lesser mantissa
-    wire [22:0] nm; // Normalized mantissa
-    wire [5:0] sum;
-    wire carry;
-    wire overflow;
-    wire [2:0] final_exponent;
-    wire [4:0] final_mantissa;
-    wire [5:0] normalized_sum;
+    wire [23:0] nm; // Normalized mantissa
+    wire [24:0] sm; // Sum of normalized mantissas
+    wire ovf; // Overflow
+    wire carry; // Carry
+    wire [7:0] fe; // Final exponent
+    wire [22:0] fm; // Final mantissa
+    wire [22:0] ns; // Normalized sum
 
     // Determine larger number
-    assign ge = a[30:23] >= b[30:23] ? a : b;
-    assign le = b[30:23] > a[30:23] ? b : a;
+    assign gn = a[30:23] >= b[30:23] ? a : b;
+    assign ln = b[30:23] < a[30:23] ? b : a;
+    assign ge = gn[30:23];
+    assign le = ln[30:23];
+    assign gm = gn[22:0];
+    assign lm = ln[22:0];
 
     // Normalize the smaller number
-    shifter normalize (.in(lesser_mantissa), .distance(greater_exponent - lesser_exponent), .direction(1'b1), .out(normalized_mantissa));
+    assign nm = {|le,lm} >> (ge - le);
 
     // Add normalized mantissa
-    adder add(.a(normalized_mantissa), .b(greater_mantissa), .sum(sum[4:0]), .cout(carry));
-    assign sum[5] = carry;
-    assign normalized_sum = sum >> carry;
-    assign overflow = &greater_exponent & carry;
+    assign sm = nm + {|ge, gm};
+    
+    // Normalize sum
+    assign carry = sm[24];
+    assign nm = sm >> carry;
+    
+    // Check for overflow
+    assign ovf = &ge & carry;
 
     // Deal with overflow case
-    assign final_exponent = overflow ? greater_exponent : greater_exponent + carry;
-    assign final_mantissa = overflow ? 5'b11111 : normalized_sum[4:0];
-    assign result = {final_exponent, final_mantissa};
-endmodule
-
-    
+    assign fe = ovf ? ge : ge + carry;
+    assign fm = ovf ? 23'b11111111111111111111111 : nm[22:0];
+    assign out = {in[31],fe, fm};
 endmodule
